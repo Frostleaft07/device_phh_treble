c24660ba0abcdfc1539aeccd146242aceb2d4e1f
Frostleaft07 <zx7unknow@gmail.com>
2024-09-18 05:39:11 +0000

    initial crdroid
    Signed-off-by: Frostleaft07 <zx7unknow@gmail.com>

---
+++ treble_arm64_bvN.mk
@@ -0,0 +1,16 @@
+TARGET_GAPPS_ARCH := arm64
+$(call inherit-product, device/phh/treble/base-pre.mk)
+include build/make/target/product/aosp_arm64_ab.mk
+$(call inherit-product, vendor/vndk/vndk.mk)
+$(call inherit-product, device/phh/treble/base.mk)
+
+
+$(call inherit-product, device/phh/treble/crdroid.mk)
+
+PRODUCT_NAME := Nightmare
+PRODUCT_DEVICE := nightmare
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := nightmare
+
+PRODUCT_PACKAGES += 
+
